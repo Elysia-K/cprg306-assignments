f6b662f151af7af6b1a33d171d31d67f9bf6e32d
Elysia-K <tjsgml0806@gmail.com>
2024-09-13 13:59:09 -0600

    Week-2 assignmnet feature in it
    finished function for the step 1, 2, 3, 4

---
+++ app/week-2/student-info.js
@@ -0,0 +1,13 @@
+import Link from 'next/link';
+
+export default function StudentInfo() {
+  return (
+    <div>
+      <h1>StudentInfo</h1>
+      <p>Student Name: Sunhee Ku</p>
+      <Link href="https://github.com/Elysia-K/cprg306-assignments">
+        Go To Sunhee's Github
+      </Link>
+    </div>
+  );
+}
