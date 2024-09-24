4f9e72b3160772643d3f4b6af20045dab4e64391
Elysia-K <tjsgml0806@gmail.com>
2024-09-24 10:41:09 -0600

    Week-3 assignment
    Week-3 assignment step 1 ~ 4

--- app/page.js
+++ app/page.js
@@ -3,8 +3,13 @@ import Link from 'next/link';
 export default function Home() {
   return (
     <main>
-      <h1>CPRG 306: Web Development 2 - Assignments</h1>
-      <Link href="./week-2">this is the link for week2 assignments</Link>
+      <h1 className="text-2xl">CPRG 306: Web Development 2 - Assignments</h1>
+      <p>
+        <Link href="./week-2">this is the link for week2 assignments</Link>
+      </p>
+      <p>
+        <Link href="./week-3">Shopping List</Link>
+      </p>
     </main>
   );
 }
